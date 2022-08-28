290eb27aa6087d2b0249a348c8bd96862ef78178
kat-5 <catalinaolend@gmail.com>
2022-08-27 22:09:38 -0700

    Feature SUN added but with Errors

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,3 +1,5 @@
+package com.company;
+
 public class Main {
     public static void main(String[] args) {
 
@@ -10,5 +12,11 @@ public class Main {
         inception.Planets=2;
 
         System.out.println("The basic structure is ready");
+
+        SUN omaro=new SUN();
+        omaro.temperature="123346575j";
+        omaro.name="ferrro";
+        omaro.radius=123344445555;
+        omaro.color="pink";
     }
 }
