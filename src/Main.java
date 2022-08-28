21bfdb5fa7dddf339bc2acb42b0899624d799329
kat-5 <catalinaolend@gmail.com>
2022-08-27 21:44:38 -0700

    Solar system is created

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        //write your code here
+
+        //commit before you switch branch
+
+        solarSystem inception= new solarSystem();
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/solarSystem.java
@@ -0,0 +1,7 @@
+public class solarSystem {
+
+    int Sun;
+    int Moon;
+    int Planets;
+
+}
